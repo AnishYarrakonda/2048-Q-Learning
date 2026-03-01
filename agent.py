abf683f7325519e55889206b51ce682c2560aa06
Anish Yarrakonda <anish.yarrakonda456@gmail.com>
2026-02-28 21:37:27 -0800

    sdfs

--- agent.py
+++ agent.py
@@ -1,11 +1,9 @@
 # imports
+import random
+from board import Board
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
-import random
-import numpy as np
-from board import Board
 
 # use gpu for faster operations
 device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
